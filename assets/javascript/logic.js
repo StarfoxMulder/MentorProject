2a37efda8fe7e3950902e5a2c4fa6c54639e7de3
Charles Prestia <Charles Prestia>
2016-09-02 21:51:42 -0400

    Haven't pushed in a while - videos are now in cards, been working on styling/positioning them correctly while playing around with the UI Andrea found

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -48,12 +48,85 @@ $("#menu").on("click", function(){
 });
 //////END FRONT END JS///////
 
-
 //clears search box content//
 
 	$(".clearText").on("click", function() {
 		$("#theme-form input").val("");
 	});
+
+
+// Toggle display of media div so that it's 20% at default,
+//	but expands to 60% and displays video players on 'Videos' click
+	$('body').on('click', '#videos', function() {
+		console.log("video click event listener is working");
+
+		if ($("#media").attr("height") == "20%") {
+			$("#media").attr("height", "70%");
+			$("#videoContainer").attr("display", "block");
+		} else if ($("#media").attr("height") == "70%") {
+			$("#media").attr("height", "20%");
+			$("#videoContainer").attr("display", "none");
+		}
+	});
+
+
+//////// YouTube API ////////
+
+/* YouTube API Key
+AIzaSyAVbBQ2P3x0mUGE0xRfvkx0JLd5trxp2as
+*/
+
+	$('#searchTopic').on("click", function() {
+		var queryTerm = $("#search-input").val();
+		var queryURL = "https://www.googleapis.com/youtube/v3";
+
+
+
+		/* loads a video's thumbnail and prepares the player to play the video, 
+		    	but does not play it until playVideo() is called */
+
+	/*	player.cueVideoById({videoId:String,
+                     startSeconds:Number,
+                     endSeconds:Number,
+                     suggestedQuality:String}):Void */
+
+
+	});
+
+	// Having a clicked smaller video populate in the larger player //
+	$('.smallVid').on("click", function() {
+		var currentVid = $(this).attr('src');
+
+		var tag = document.createElement('script');
+
+		tag.src = "https://www.youtube.com/iframe_api";
+		var firstScriptTag = document.getElementByTagName('script')[0];
+		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+		var player;
+		function onYouTubeIframeAPIReady() {
+			player = new YT.Player('player', {
+				height: '390',
+				width: '640',
+				videoID: "M71c1UVf-VE",
+				events: {
+					'onReady': onPlayerReady,
+					//'onStateChange': onPlayerStateChange
+				}
+			});
+		}
+	});
+
+	//Get targeted player to play
+	function onPlayerReady(event){
+		event.target.playVideo();
+	};
+
+	//Get targeted video to pause
+/*	function pauseVideo() {
+		player.pauseVideo();
+	}
+*/
 
 
 });
